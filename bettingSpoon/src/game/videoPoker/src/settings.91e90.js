bf76c2d69a6e3a28a63eb7c0bbdf879675fd5d4e
ahyoung <ahyoung.db@gmail.com>
2022-03-07 17:36:47 +0900

    게임소스 업데이트

---
+++ bettingSpoon/src/game/videoPoker/src/settings.91e90.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["ball","wall"],collisionMatrix:[[true,true],[true,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",resources:"71ef6",main:"a6058"}};
